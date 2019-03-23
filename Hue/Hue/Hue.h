e3703b2b55f013833b81b922632cba746649e7f5
Adam Graham <adam.zigurous@gmail.com>
2019-03-22 19:31:25 -0500

    Create empty project

---
+++ Hue/Hue/Hue.h
@@ -0,0 +1,18 @@
+//
+//  Hue.h
+//  Hue
+//
+//  Created by Adam Graham on 3/11/19.
+//  Copyright © 2019 Adam Graham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hue.
+FOUNDATION_EXPORT double HueVersionNumber;
+
+//! Project version string for Hue.
+FOUNDATION_EXPORT const unsigned char HueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hue/PublicHeader.h>
