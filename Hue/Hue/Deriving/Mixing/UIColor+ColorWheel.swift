c2d37a73887b3b4697c2ac210f6a21771d13cdab
Adam Graham <adam.zigurous@gmail.com>
2019-04-19 16:49:37 -0500

    Add UIColor+ColorWheel

---
+++ Hue/Hue/Deriving/Mixing/UIColor+ColorWheel.swift
@@ -0,0 +1,99 @@
+//
+//  UIColor+ColorWheel.swift
+//  Hue
+//
+//  Created by Adam Graham on 4/19/19.
+//  Copyright © 2019 Adam Graham. All rights reserved.
+//
+
+import UIKit
+
+/// An extension to declare the colors of the color wheel.
+public extension UIColor {
+
+    /// The colors that define the color wheel.
+    public struct ColorWheel {
+
+        /// The colors that define the RGB color wheel.
+        public struct RGB {
+
+            /// `#ffffff`, `rgb(255,255,255)`
+            public static let white = UIColor.white
+            /// `#000000`, `rgb(0,0,0)`
+            public static let black = UIColor.black
+
+            /// `#ff0000`, `rgb(255,0,0)`
+            public static let red = UIColor.red
+            /// `#00ff00`, `rgb(0,255,0)`
+            public static let green = UIColor.green
+            /// `#0000ff`, `rgb(0,0,255)`
+            public static let blue = UIColor.blue
+
+            /// `#00ffff`, `rgb(0,255,255)`
+            public static let cyan = UIColor.cyan
+            /// `#ff00ff`, `rgb(255,0,255)`
+            public static let magenta = UIColor.magenta
+            /// `#ffff00`, `rgb(255,255,0)`
+            public static let yellow = UIColor.yellow
+
+            /// `#0080ff`, `rgb(0,128,255)`
+            public static let azure = cyan + blue
+            /// `#8000ff`, `rgb(128,0,255)`
+            public static let violet = blue + magenta
+            /// `#ff0080`, `rgb(255,0,128)`
+            public static let rose = magenta + red
+            /// `#ff8000`, `rgb(255,128,0)`
+            public static let orange = red + yellow
+            /// `#80ff00`, `rgb(128,255,0)`
+            public static let chartreuse = yellow + green
+            /// `#00ff80`, `rgb(0,255,128)`
+            public static let springGreen = green + cyan
+
+            private init() {}
+
+        }
+
+        /// The colors that define the RYB color wheel.
+        public struct RYB {
+
+            /// `#ffffff`, `ryb(255,255,255)`
+            public static let white = UIColor.white
+            /// `#000000`, `ryb(0,0,0)`
+            public static let black = UIColor.black
+
+            /// `#ff0000`, `ryb(255,0,0)`
+            public static let red = UIColor.red
+            /// `#00ff00`, `ryb(0,255,0)`
+            public static let yellow = UIColor.yellow
+            /// `#0000ff`, `ryb(0,0,255)`
+            public static let blue = UIColor.blue
+
+            /// `#00ffff`, `ryb(0,255,255)`
+            public static let green = UIColor.green
+            /// `#ff00ff`, `ryb(255,0,255)`
+            public static let purple = UIColor.magenta
+            /// `#ffff00`, `ryb(255,255,0)`
+            public static let orange = UIColor.orange
+
+            /// `#ff8000`, `ryb(255,128,0)`
+            public static let vermilion = red + orange
+            /// `#80ff00`, `ryb(128,255,0)`
+            public static let amber = orange + yellow
+            /// `#00ff80`, `ryb(0,255,128)`
+            public static let chartreuse = yellow + green
+            /// `#0080ff`, `ryb(0,128,255)`
+            public static let teal = green + blue
+            /// `#8000ff`, `ryb(128,0,255)`
+            public static let violet = blue + purple
+            /// `#ff0080`, `ryb(255,0,128)`
+            public static let magenta = purple + red
+
+            private init() {}
+
+        }
+
+        private init() {}
+
+    }
+
+}
