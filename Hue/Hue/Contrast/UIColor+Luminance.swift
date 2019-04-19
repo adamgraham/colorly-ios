425f40d29eae68e85df83b83a575dfd67916709b
Adam Graham <adam.zigurous@gmail.com>
2019-04-19 16:48:06 -0500

    Update luminance functions with 0-1 linear ranges

--- Hue/Hue/Contrast/UIColor+Luminance.swift
+++ Hue/Hue/Contrast/UIColor+Luminance.swift
@@ -13,25 +13,34 @@ public extension UIColor {
 
     /// The relative luminance of the color, measured according to the HSL color model.
     var luminance: CGFloat {
-        let rgb = self.rgb
+        var (r, g, b) = (CGFloat(), CGFloat(), CGFloat())
+        getRed(&r, green: &g, blue: &b, alpha: nil)
 
         // converts an RGB component to Linear RGB
         func linear(_ component: CGFloat) -> CGFloat {
             return (component > 0.03928) ? pow((component + 0.055) / 1.055, 2.4) : (component / 12.92)
         }
 
-        return 0.2126 * linear(rgb.red) +
-               0.7152 * linear(rgb.green) +
-               0.0722 * linear(rgb.blue)
+        return 0.2126 * r +
+               0.7152 * g +
+               0.0722 * b
     }
 
     /// The perceived brightness of the color, measured according to the HSP color model -
     /// http://alienryderflex.com/hsp.html.
     var perceivedBrightness: CGFloat {
-        let rgb = self.rgb
-        let r = 0.299 * pow(rgb.red, 2.0)
-        let g = 0.587 * pow(rgb.green, 2.0)
-        let b = 0.114 * pow(rgb.blue, 2.0)
+        var (r, g, b) = (CGFloat(), CGFloat(), CGFloat())
+        getRed(&r, green: &g, blue: &b, alpha: nil)
+
+        // converts an RGB component to Linear RGB
+        func linear(_ component: CGFloat) -> CGFloat {
+            return (component > 0.03928) ? pow((component + 0.055) / 1.055, 2.4) : (component / 12.92)
+        }
+
+        r = 0.299 * pow(linear(r), 2.0)
+        g = 0.587 * pow(linear(g), 2.0)
+        b = 0.114 * pow(linear(b), 2.0)
+
         return sqrt(r + g + b)
     }
 
