c8e78ca568c769bb7f57ae4340543d5267c5aaca
Adam Graham <adam.zigurous@gmail.com>
2019-05-04 15:21:06 -0500

    Fix LAB conversion calculations and normalization

--- Hue/Hue/Models/Representations/UIColor+LAB.swift
+++ Hue/Hue/Models/Representations/UIColor+LAB.swift
@@ -11,8 +11,17 @@ import UIKit
 /// An extension to provide conversion to and from CIE-LAB colors.
 public extension UIColor {
 
-    /// Standard daylight illuminate, sRGB, Adobe-RGB
-    private static let d65: (x: CGFloat, y: CGFloat, z: CGFloat) = (0.9504, 1.0000, 1.0888)
+    /// A set of constant values used to convert to and from CIE-LAB colors.
+    private struct Constant {
+
+        static let ⅓: CGFloat = 1.0 / 3.0
+        static let ⁴୵₂₉: CGFloat = 4.0 / 29.0
+        static let δ: CGFloat = 6.0 / 29.0
+        static let δ² = δ * δ
+        static let δ³ = δ * δ * δ
+        static let δ²3 = δ * δ * 3.0
+
+    }
 
     /// The components of a color in the CIE-LAB color model.
     ///
@@ -28,23 +37,20 @@ public extension UIColor {
     /// The components of the color in the CIE-LAB color model.
     var lab: LAB {
         let fn = { (t: CGFloat) -> CGFloat in
-            if t > 0.008856 {
-                return pow(t, 1.0 / 3.0)
-            } else {
-                return (7.787 * t) + (16.0 / 116.0)
-            }
+            if t > Constant.δ³ { return pow(t, Constant.⅓) }
+            return (t / Constant.δ²3) + Constant.⁴୵₂₉
         }
 
-        let r = UIColor.d65
+        let r = UIColor.Illuminant.d65.twoDegrees
         let xyz = self.xyz
 
-        let rx = fn(xyz.x / r.x)
-        let ry = fn(xyz.y / r.y)
-        let rz = fn(xyz.z / r.z)
+        let fx = fn(xyz.x / 100.0 / r.x)
+        let fy = fn(xyz.y / 100.0 / r.y)
+        let fz = fn(xyz.z / 100.0 / r.z)
 
-        let L = (116.0 * ry) - 16.0
-        let a = 500.0 * (rx - ry)
-        let b = 200.0 * (ry - rz)
+        let L = (116.0 * fy) - 16.0
+        let a = 500.0 * (fx - fy)
+        let b = 200.0 * (fy - fz)
 
         return LAB(L: L, a: a, b: b)
     }
@@ -52,23 +58,18 @@ public extension UIColor {
     /// Initializes a color from the components of a CIE-LAB color model.
     convenience init(lab: LAB, alpha: CGFloat = 1.0) {
         let fn = { (t: CGFloat) -> CGFloat in
-            let t³ = pow(t, 3.0)
-            if t³ > 0.008856 {
-                return t³
-            } else {
-                return (t - (16.0 / 116.0)) / 7.787
-            }
+            if t > Constant.δ { return pow(t, 3.0) }
+            return Constant.δ²3 * (t - Constant.⁴୵₂₉)
         }
 
-        let r = UIColor.d65
+        let r = UIColor.Illuminant.d65.twoDegrees
+        let L = (lab.L + 16.0) / 116.0
+        let a = L + (lab.a / 500.0)
+        let b = L - (lab.b / 200.0)
 
-        var y = (lab.L + 16.0) / 116.0
-        var x = (lab.a / 500.0) + y
-        var z = y - (lab.b / 200.0)
-
-        x = fn(x) * r.x
-        y = fn(y) * r.y
-        z = fn(z) * r.z
+        let x = r.x * fn(a) * 100.0
+        let y = r.y * fn(L) * 100.0
+        let z = r.z * fn(b) * 100.0
 
         self.init(xyz: XYZ(x: x, y: y, z: z), alpha: alpha)
     }
