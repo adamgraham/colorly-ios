fd9e75bcd38b03187e07d4e6dc5c7a820d67d00c
Adam Graham <adam.zigurous@gmail.com>
2019-05-06 21:05:09 -0500

    Unit test UIColor+Averaging

--- Hue/HueTests/Deriving/Averaging/UIColor+AveragingTest.swift
+++ Hue/HueTests/Deriving/Averaging/UIColor+AveragingTest.swift
@@ -13,7 +13,10 @@ import XCTest
 class UIColor_AveragingTest: XCTestCase {
 
     func testAverage() {
-
+        XCTAssertEqual(UIColor.average(of: .white, and: .black), UIColor(red: 0.7071067811865476, green: 0.7071067811865476, blue: 0.7071067811865476, alpha: 1.0))
+        XCTAssertEqual(UIColor.average(of: [.red, .green, .blue]), UIColor(red: 0.5773502691896257, green: 0.5773502691896257, blue: 0.5773502691896257, alpha: 1.0))
+        XCTAssertEqual(UIColor.average(of: [.cyan, .magenta, .yellow]), UIColor(red: 0.816496580927726, green: 0.816496580927726, blue: 0.816496580927726, alpha: 1.0))
+        XCTAssertEqual(UIColor.average(of: [.red, .green, .blue, .cyan, .magenta, .yellow]), UIColor(red: 0.7071067811865476, green: 0.7071067811865476, blue: 0.7071067811865476, alpha: 1.0))
     }
 
 }
