4b0f40fb9850216a1f693409a062d8dd7f91bdf2
Adam Graham <adam.zigurous@gmail.com>
2019-05-06 21:07:53 -0500

    Unit test UIColor+Mixing

--- Hue/HueTests/Deriving/Mixing/UIColor+MixingTest.swift
+++ Hue/HueTests/Deriving/Mixing/UIColor+MixingTest.swift
@@ -13,7 +13,15 @@ import XCTest
 class UIColor_MixingTest: XCTestCase {
 
     func testMixing() {
-
+        XCTAssertEqual(UIColor.white.mixed(with: .black), .gray)
+        XCTAssertEqual(UIColor.white.mixed(with: .black, weight: 0.0), .white)
+        XCTAssertEqual(UIColor.white.mixed(with: .black, weight: 1.0), .black)
+        XCTAssertEqual(UIColor.cyan.mixed(with: .blue), .azure)
+        XCTAssertEqual(UIColor.blue.mixed(with: .magenta), .violet)
+        XCTAssertEqual(UIColor.magenta.mixed(with: .red), .rose)
+        XCTAssertEqual(UIColor.red.mixed(with: .yellow), .orange)
+        XCTAssertEqual(UIColor.yellow.mixed(with: .green), .chartreuse)
+        XCTAssertEqual(UIColor.green.mixed(with: .cyan), .springGreen)
     }
 
 }
