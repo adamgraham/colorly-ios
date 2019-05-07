5ecc4bfe0fa96a66701e1d8d8c576e1865330118
Adam Graham <adam.zigurous@gmail.com>
2019-05-06 21:08:15 -0500

    Unit test UIColor+CMYK

--- Hue/HueTests/Models/Representations/UIColor+CMYKTest.swift
+++ Hue/HueTests/Models/Representations/UIColor+CMYKTest.swift
@@ -12,12 +12,36 @@ import XCTest
 
 class UIColor_CMYKTest: XCTestCase {
 
-    func testCMYK() {
+    typealias CMYK = UIColor.CMYK
 
+    func testCMYK() {
+        XCTAssertEqual(UIColor.white.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 0.0))
+        XCTAssertEqual(UIColor.red.cmyk, CMYK(cyan: 0.0, magenta: 1.0, yellow: 1.0, black: 0.0))
+        XCTAssertEqual(UIColor.green.cmyk, CMYK(cyan: 1.0, magenta: 0.0, yellow: 1.0, black: 0.0))
+        XCTAssertEqual(UIColor.blue.cmyk, CMYK(cyan: 1.0, magenta: 1.0, yellow: 0.0, black: 0.0))
+        XCTAssertEqual(UIColor.cyan.cmyk, CMYK(cyan: 1.0, magenta: 0.0, yellow: 0.0, black: 0.0))
+        XCTAssertEqual(UIColor.magenta.cmyk, CMYK(cyan: 0.0, magenta: 1.0, yellow: 0.0, black: 0.0))
+        XCTAssertEqual(UIColor.yellow.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 1.0, black: 0.0))
+        XCTAssertEqual(UIColor.black.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 1.0))
+        XCTAssertEqual(UIColor.darkGray.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 0.6666666666666667))
+        XCTAssertEqual(UIColor.gray.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 0.5))
+        XCTAssertEqual(UIColor.lightGray.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 0.33333333333333337))
+        XCTAssertEqual(UIColor.clear.cmyk, CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 1.0))
     }
 
     func testCMYKInit() {
-
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 0.0)), .white)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 1.0, yellow: 1.0, black: 0.0)), .red)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 1.0, magenta: 0.0, yellow: 1.0, black: 0.0)), .green)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 1.0, magenta: 1.0, yellow: 0.0, black: 0.0)), .blue)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 1.0, magenta: 0.0, yellow: 0.0, black: 0.0)), .cyan)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 1.0, yellow: 0.0, black: 0.0)), .magenta)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 1.0, black: 0.0)), .yellow)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 1.0)), .black)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 2.0/3.0)), .darkGray)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 0.5)), .gray)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 1.0/3.0)), .lightGray)
+        XCTAssertEqual(UIColor(cmyk: CMYK(cyan: 0.0, magenta: 0.0, yellow: 0.0, black: 1.0), alpha: 0.0), .clear)
     }
 
 }
