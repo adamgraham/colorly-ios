fb19b4025abe20a517d51568b6c657c8fa7db6cb
Adam Graham <adam.zigurous@gmail.com>
2019-05-06 21:04:55 -0500

    Unit test UIColor+Lightness

--- Hue/HueTests/Deriving/Adjusting/UIColor+LightnessTest.swift
+++ Hue/HueTests/Deriving/Adjusting/UIColor+LightnessTest.swift
@@ -13,11 +13,35 @@ import XCTest
 class UIColor_LightnessTest: XCTestCase {
 
     func testLighter() {
-
+        XCTAssertEqual(UIColor.black.lighter(percent: -1.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.1), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.1, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.2), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.2, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.3), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.3, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.4), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.4, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.5), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.5, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.6), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.6, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.7), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.7, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.8), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.8, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 0.9), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.9, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 1.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 1.0, alpha: 1.0))
+        XCTAssertEqual(UIColor.black.lighter(percent: 2.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 1.0, alpha: 1.0))
     }
 
     func testDarker() {
-
+        XCTAssertEqual(UIColor.white.darker(percent: -1.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 1.0, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 1.0, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.1), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.9, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.2), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.8, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.3), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.7, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.4), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.6, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.5), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.5, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.6), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.4, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.7), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.3, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.8), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.2, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 0.9), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.1, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 1.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 1.0))
+        XCTAssertEqual(UIColor.white.darker(percent: 2.0), UIColor(hue: 0.0, saturation: 0.0, brightness: 0.0, alpha: 1.0))
     }
 
 }
