b0da05ec427b17e833d503903b09a74b76749734
Adam Graham <adam.zigurous@gmail.com>
2019-05-06 21:09:14 -0500

    Unit test Functions

--- Hue/HueTests/Utils/FunctionsTest.swift
+++ Hue/HueTests/Utils/FunctionsTest.swift
@@ -13,11 +13,27 @@ import XCTest
 class FunctionsTest: XCTestCase {
 
     func testClamp() {
-
+        XCTAssertEqual(clamp(-0.5, 0.0, 1.0), 0.0)
+        XCTAssertEqual(clamp(-0.25, 0.0, 1.0), 0.0)
+        XCTAssertEqual(clamp(0.0, 0.0, 1.0), 0.0)
+        XCTAssertEqual(clamp(0.25, 0.0, 1.0), 0.25)
+        XCTAssertEqual(clamp(0.5, 0.0, 1.0), 0.5)
+        XCTAssertEqual(clamp(0.75, 0.0, 1.0), 0.75)
+        XCTAssertEqual(clamp(1.0, 0.0, 1.0), 1.0)
+        XCTAssertEqual(clamp(1.25, 0.0, 1.0), 1.0)
+        XCTAssertEqual(clamp(1.5, 0.0, 1.0), 1.0)
     }
 
     func testRotatingClamp() {
-
+        XCTAssertEqual(rotatingClamp(-0.5, 0.0, 1.0), 0.5)
+        XCTAssertEqual(rotatingClamp(-0.25, 0.0, 1.0), 0.75)
+        XCTAssertEqual(rotatingClamp(0.0, 0.0, 1.0), 0.0)
+        XCTAssertEqual(rotatingClamp(0.25, 0.0, 1.0), 0.25)
+        XCTAssertEqual(rotatingClamp(0.5, 0.0, 1.0), 0.5)
+        XCTAssertEqual(rotatingClamp(0.75, 0.0, 1.0), 0.75)
+        XCTAssertEqual(rotatingClamp(1.0, 0.0, 1.0), 1.0)
+        XCTAssertEqual(rotatingClamp(1.25, 0.0, 1.0), 0.25)
+        XCTAssertEqual(rotatingClamp(1.5, 0.0, 1.0), 0.5)
     }
 
 }
